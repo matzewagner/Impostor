02421c9fee4d212611d6b4102a7e76710bc057e3
Matthias Wagner <matzewagner@gmail.com>
2017-02-07 11:07:44 -0800

    added git ignore for /build and /node_modules

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  // this is from were webpack starts looking for files
+  context: path.join(__dirname, '/app'),
+  entry: './js/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, '/build'),
+  },
+  resolve: {
+    /**
+     * find 'import' and 'require' references
+     * with the following extensions:
+     * (may be broken in v2.2.1)
+     */
+    // extensions: ['', '.js', '.jsx', '.json'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/, // look for .js and .jsx
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      }, 
+    ],
+  },
+};
