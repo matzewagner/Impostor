b7708b8a2eb7b4d09b8dd72a2c072b768319d9b7
Matthias Wagner <matzewagner@gmail.com>
2017-02-07 20:31:59 -0800

    getting audio data, need to parse in react because of AudioContext

--- app/server/freesoundController.js
+++ app/server/freesoundController.js
@@ -1,14 +1,19 @@
 const request = require('request');
+const AudioSource = require('audiosource');
 
 const clientID = 'swwwrpFUO3In7BtmIN6j';
 const apiKey = 'ryowJnmxYqckzDz7DO3lbqKHhJMbUJiHubG030C5';
-const baseUrl = 'http://www.freesound.org/apiv2';
+const baseURI = 'http://www.freesound.org/apiv2';
 const options = '/sounds/8810';
 const requestFreesoundAccessURI = `https://www.freesound.org/apiv2/oauth2/authorize/?client_id=${clientID}&response_type=code`;
-const requestAccessTokenURI = 'https://www.freesound.org/apiv2/oauth2/access_token/';    
-const query = `${baseUrl}${options}`;
+const requestAccessTokenURI = 'https://www.freesound.org/apiv2/oauth2/access_token/';
+const query = `${baseURI}${options}`;
+
+const context = null;
+let auth;
 let accessToken;
 let data;
+let src;
 
 const freesoundController = {
   authorize: (req, res, next) => {
@@ -20,7 +25,7 @@ const freesoundController = {
     // console.log('printing code from oAuth(): ', req.query.code);
     const tokenURI = `${requestAccessTokenURI}?client_id=${clientID}&client_secret=${apiKey}&grant_type=authorization_code&code=${req.query.code}`;
 
-    request.post(tokenURI, (err, res, body) => {
+    request.post(tokenURI, (err, response, body) => {
       // console.log('getting access_token...', JSON.parse(body));
       accessToken = JSON.parse(body).access_token;
       next();
@@ -29,20 +34,33 @@ const freesoundController = {
   getData: (req, res, next) => {
     // console.log('queryUrl: ', query);
     console.log('getting data ...');
-    let auth = `Bearer ${accessToken}`;
-    const p1 = new Promise((resolve, reject) => {
-      request(
-        {
-          url: query,
-          headers: {
-            'Authorization': auth,
-          },
-        },
-        (err, response, body) => {
+    auth = `Bearer ${accessToken}`;
+    request({ url: query, headers: { Authorization: auth } },
+      (err, response, body) => {
         if (err) console.log('Error: ', err);
-        console.log('freesound response body: ', JSON.parse(body));
+        data = JSON.parse(body);
+        console.log('data: ', data);
+        next();
       });
+  },
+  getSound: (req, res, next) => {
+    const soundURI = data.previews['preview-hq-mp3'];
+    // const soundURI = data.download;
+    src = new AudioSource({
+      context: context,
+      url: soundURI,
     });
+
+    src.load(null, (err, src) => {
+      if (err) console.log(err);
+      console.log('source: ', src);
+    });
+
+    // console.log('soundURI: ', soundURI);
+    // request({ url: soundURI, headers: { Authorization: auth } }, (err, res, body) => {
+    //   if (err) console.log(err);
+    //   console.log('are we getting samples? ', res);
+    // });
     next();
   },
 };
--- app/server/server.js
+++ app/server/server.js
@@ -13,7 +13,7 @@ app.get('/fs', freesound.authorize);
  * /oauth-redirect is set as a redirect_uri on freesound.org
  * in the api key settings
  */
-app.get('/oauth-redirect', freesound.setToken, freesound.getData);
+app.get('/oauth-redirect', freesound.setToken, freesound.getData, freesound.getSound);
 
 app.listen(3000, () => {
   console.log('server listening on port 3000');
